3a01c22c0865265cf09415af27b5fec2a15982ef
varian@oneskyapp.com <Kn4Jj7PtqQak>
2015-04-24 12:15:26 +0800

    Create Product Action

--- src/Company/Bundle/DataBundle/Controller/DefaultController.php
+++ src/Company/Bundle/DataBundle/Controller/DefaultController.php
@@ -2,12 +2,29 @@
 
 namespace Company\Bundle\DataBundle\Controller;
 
+use Company\Bundle\DataBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('DataBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+        $product->setDescription('Lorem ipsum dolor');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($product);
+        $em->flush();
+
+        return new Response('Created product id '.$product->getId());
     }
 }
